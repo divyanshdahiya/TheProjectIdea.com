4f9c8de6856c18ea25f1213a6dce831f87829328
divyanshdahiya <dahiya.divyansh1@gmail.com>
2022-02-20 22:51:32 +0530

    all projects page designed

--- src/components/AllProjects.jsx
+++ src/components/AllProjects.jsx
@@ -1,17 +1,38 @@
 import React from "react";
 import ProjectCard from "./ProjectCard";
+import "./styles/allProjects.css";
+import Image from "../images/allprojects-header.png";
 
 function AllProjects({ ListOfProjects }) {
   return (
-    <div>
-      {ListOfProjects.map((data) => {
-        return (
-          <>
-            <ProjectCard data={data} />
-          </>
-        );
-      })}
-    </div>
+    <>
+      <div className="allProjects-container">
+        <div className="allProjects-content">
+          <div>
+            <img className="allProjects-img" src={Image} alt="Logo" />
+          </div>
+          <div>
+            <h1>Best Web Development Projects</h1>
+            <p>
+              Bridge the gap between theory and real-world code by working on
+              curated web development projects. Build projects to make your
+              portfolio stand out from others. From beginner web development
+              projects to more advanced ones, find your next coding project
+              now!!
+            </p>
+          </div>
+        </div>
+        <div className="allProjects-card">
+          {ListOfProjects.map((data) => {
+            return (
+              <>
+                <ProjectCard data={data} />
+              </>
+            );
+          })}
+        </div>
+      </div>
+    </>
   );
 }
 
